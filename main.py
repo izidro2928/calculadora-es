24dfa52721ee0157a3a9d76c618781a94d5a7835
Isidro Gonzalez <31930997+izidro2928@users.noreply.github.com>
2021-11-15 02:43:37 +0000

    Más operaciones

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
 def sumar(x, y):
-  return x + y
+    return x + y
+
+def restar(x, y):
+    return x - y
+
+def multiplicar(x, y):
+    return x * y
+
+def dividir(x, y):
+    return x / y
+
+print("Porfavor elige una operacion")
+print("1.sumar")
+print("2.restar")
+print("3.multiplicar")
+print("4.dividir")
 
 while True:
 
-  operacion = input("Porfavor indrodusca una operacion: ")
+    operacion = input("Porfavor indrodusca una operacion: ")
 
-  if operacion in ('1', '2', '3', '4'):
+    if operacion in ('1', '2', '3', '4'):
 
-    numero1 = float(input("Porfavor introdusca el primer numero: "))
-    numero2 = float(input("Porfavor introdusca el segundo numero: "))
+        numero1 = float(input("Porfavor introdusca el primer numero: "))
+        numero2 = float(input("Porfavor introdusca el segundo numero: "))
 
-    if operacion == "1":
-      print(numero1, "+", numero2, "=", sumar(numero1, numero2))
+        if operacion == "1":
+            print(numero1, "+", numero2, "=", sumar(numero1, numero2))
 
-    siguente_operacion = input("Deseas hacer mas operaciones (si/no): ")
+        elif operacion == "2":
+            print(numero1, "-", numero2, "=", restar(numero1, numero2))
 
-    if siguente_operacion == "no":
-      break
+        elif operacion == "3":
+            print(numero1, "*", numero2, "=", multiplicar(numero1, numero2))
+
+        elif operacion == "4":
+            print(numero1, "/", numero2, "=", dividir(numero1, numero2))
+
+        siguente_operacion = input("Deseas hacer mas operaciones (si/no): ")
+
+        if siguente_operacion == "no":
+            print("Gracias por utilizar mi programa :)")
+            break
+    else:
+        print("Error: Porfavor introduce una operacion valida!")
